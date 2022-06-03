af9bba63ceb5017250e9b774ed3b507462f14fb9
aselwilliams <aselwilliams22@gmail.com>
2022-06-03 18:32:39 -0400

    changes in structure

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Form,Row,Col,FloatingLabel,Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [user, setUser]=useState({firstName:'',lastName:'',email:'', phone:'',password:'',confirmPwd:'', registerCode:'', state: '',avatar:'' })
   return (
     <div className="container">
 <header>
@@ -15,12 +16,12 @@ function App() {
   <Row className="g-2 mb-4">
   <Col md className='m-3'>
     <FloatingLabel controlId="floatingInputGrid" label="Name *">
-      <Form.Control type="text" placeholder="Name *" />
+      <Form.Control type="text" placeholder="Name *" id='firstName' name='firstName' value={user.firstName}/>
     </FloatingLabel>
   </Col>
   <Col md className='m-3'>
     <FloatingLabel controlId="floatingInputGrid" label="Last Name *">
-      <Form.Control type="text" placeholder="Last Name *" />
+      <Form.Control type="text" placeholder="Last Name *" id='lastName' name='lastName' value={user.lastName} />
     </FloatingLabel>
   </Col>
   </Row>
@@ -28,12 +29,12 @@ function App() {
   <Row className="g-2 mb-4">
   <Col md className='m-3'>
     <FloatingLabel controlId="floatingInputGrid" label="Email *">
-      <Form.Control type="email" placeholder="Email *" />
+      <Form.Control type="email" placeholder="Email *"  id='email' name='email' value={user.email} />
     </FloatingLabel>
   </Col>
   <Col md className='m-3'>
     <FloatingLabel controlId="floatingInputGrid" label="Phone *">
-      <Form.Control type="tel" placeholder="Phone *" />
+      <Form.Control type="tel" placeholder="Phone *" id='phone' name='phone' value={user.phone}/>
     </FloatingLabel>
   </Col>
   </Row>
@@ -41,12 +42,12 @@ function App() {
   <Row className="g-2 mb-4">
   <Col md className='m-3'>
     <FloatingLabel controlId="floatingInputGrid" label="Password *">
-      <Form.Control type="password" placeholder="Password *" />
+      <Form.Control type="password" placeholder="Password *" id='password' name='password' value={user.password}/>
     </FloatingLabel>
   </Col>
   <Col md className='m-3'>
     <FloatingLabel controlId="floatingInputGrid" label="Confirm Password *">
-      <Form.Control type="password" placeholder="Confirm Password *" />
+      <Form.Control type="password" placeholder="Confirm Password *" id='confirmPwd' name='confirmPwd' value={user.confirmPwd} />
     </FloatingLabel>
   </Col>
   </Row>
@@ -54,12 +55,12 @@ function App() {
   <Row className="g-2 mb-4">
   <Col md className='m-3'>
     <FloatingLabel controlId="floatingInputGrid" label="Register Code *">
-      <Form.Control type="text" placeholder="Register Code *" />
+      <Form.Control type="text" placeholder="Register Code *" id='registerCode' name='registerCode' value={user.registerCode} />
     </FloatingLabel>
   </Col>
   <Col md className='m-3'>
     <FloatingLabel controlId="floatingSelectGrid" label="Works with selects">
-      <Form.Select aria-label="Floating label select example">
+      <Form.Select aria-label="Floating label select example" id='state' name='state' value={user.state}>
         <option>State *</option>
         <option value="1">One</option>
         <option value="2">Two</option>
@@ -72,7 +73,7 @@ function App() {
 <Row className="g-2 mb-4">
   <Col md className='m-3'>
     <FloatingLabel controlId="floatingInputGrid" label="Avatar Url">
-      <Form.Control type="url" placeholder="Avatar Url" />
+      <Form.Control type="url" placeholder="Avatar Url" id='avatar' name='avatar' value={user.avatar}/>
     </FloatingLabel>
   </Col>
 </Row>
